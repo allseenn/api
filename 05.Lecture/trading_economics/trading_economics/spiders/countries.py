8c1580bfde4e5069f47ee43780ccd97d24c04b4b
allseenn <allseen@live.ru>
2024-03-17 18:59:34 +0300

    05.Lecture

---
+++ 05.Lecture/trading_economics/trading_economics/spiders/countries.py
@@ -0,0 +1,32 @@
+import scrapy
+
+class CountriesSpider(scrapy.Spider):
+    name = "countries"
+    allowed_domains = ["tradingeconomics.com"]
+    start_urls = ["https://tradingeconomics.com/country-list/inflation-rate?continent=world"]
+
+    def parse(self, response):
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get().strip()
+            link = country.xpath(".//@href").get()
+            yield response.follow(url=link, callback=self.parse_country, meta={"name": name})
+
+
+    def parse_country(self, response):
+        rows = response.xpath("//tr[contains(@class, 'datatable-row')]")
+        for row in rows:
+            related = row.xpath(".//td[1]/a/text()").get().strip()
+            #title_link = row.xpath(".//td[1]/a/@href").get()
+            last = float(row.xpath(".//td[2]/text()").get().strip())
+            try:
+                previous = float(row.xpath(".//td[3]/text()").get().strip())
+            except:
+                previous = ""
+        yield {
+                "name": response.request.meta["name"],
+                "related": related,
+                "last": last,
+                "previous": previous
+            }
+

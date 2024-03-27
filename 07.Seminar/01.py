c53d887de0cd3e4b7eff81ed516f75e74d83b6f5
allseenn <allseen@live.ru>
2024-03-27 12:45:49 +0000

    07.Seminar

--- 07.Seminar/01.py
+++ 07.Seminar/01.py
@@ -1,5 +1,6 @@
 #!/bin/python
 from libs import *
+from download import *
 
 time_start = time.time()
 #driver.get("https://www.auchan.ru")
@@ -13,63 +14,87 @@ input.send_keys(Keys.ENTER)
 
 page = 1
 cards_list = []
-
 while True:
+    time.sleep(3)
+    try:
+        wait = WebDriverWait(driver, 10)
+        cards = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//article[@id]")))  
+    except Exception as e:
+        print(e)
     while True:
-        wait = WebDriverWait(driver, 10)
-        time.sleep(4)
-        try:
-            cards = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//article[@id]")))
-            count = len(cards)
-        except:
-            break
-        driver.execute_script("window.scrollBy(0, 1500)")
-        # time.sleep(1)
+        driver.execute_script("window.scrollBy(0, 800)")
         # cards = driver.find_elements(By.XPATH, "//article[@id]")
-        wait = WebDriverWait(driver, 10)
         time.sleep(3)
         try:
-            cards = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//article[@id]")))
-        except:
-            pass
-        if count < 2:
+            wait = WebDriverWait(driver, 10)
+            cards = cards + wait.until(EC.presence_of_all_elements_located((By.XPATH, "//article[@id]")))
+        except Exception as e:
+            print(e)
+        if len(cards) < 30:
             break
+        elif len(cards) > 99:
+            driver.execute_script("window.scrollBy(0, -500)")
+            break
+    if len(cards) < 3:
+        break
 
     for card in cards:
         card_dict = {}
-        article = card.get_attribute("id")
-        card_dict["article"] = article
-        link = card.find_element(By.TAG_NAME, "a").get_attribute("href")
+        try:
+            id = card.get_attribute("id")
+            card_dict["_id"] = id
+        except:
+            continue
+        try:
+            link = card.find_element(By.TAG_NAME, "a").get_attribute("href")
+        except:
+            link = None
         card_dict["link"] = link
         try:
             price = card.find_element(By.XPATH,  './/ins[contains(@class, "price__lower-price")]').text
+            price = int(price.replace(" ", "").replace("₽", "").replace("&nbsp", "."))
         except:
             price = None
         card_dict["price"] = price
-        brand = card.find_element(By.CLASS_NAME, "product-card__brand").text
-        card_dict["brand"] = brand
-        name = card.find_element(By.CLASS_NAME, "product-card__name").text
-        card_dict["name"] = name
+        try:
+            brand = card.find_element(By.CLASS_NAME, "product-card__brand").text
+            card_dict["brand"] = brand
+        except Exception as e:
+            print(e)
+        try:
+            name = card.find_element(By.CLASS_NAME, "product-card__name").text
+            name = name.replace("/ ", "")
+            card_dict["name"] = name
+        except:
+            name = None
+        try:
+            collection.insert_one(card_dict)
+        except Exception as e:
+            print(e)
         cards_list.append(card_dict)
     
     print(f"Page {page} total {len(cards_list)} records scraped")
-    # button = 
+    #button = 
     driver.find_element(By.CLASS_NAME, "pagination-next").click()
-    # actions = ActionChains(driver)
-    # actions.scroll_to_element(button).click()
+    #actions = ActionChains(driver)
+    #actions.scroll_to_element(button).click()
     # actions.perform()
     try:
         if driver.find_element(By.XPATH, '//h1[@class="not-found-search__title"]'):
             break
     except:
         page += 1
+    if len(cards) < 3:
+        break
 
 driver.quit()
 
-with open(keyword+".csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["article", "link", "price", "brand", "name"])
-    for card in cards_list:
-        writer.writerow([card["brand"], card["name"], card["price"], card["article"], card["link"]])
-
-print(f"Найдено и сохранено {len(cards_list)} товаров в файл {keyword}.csv, за {time.time() - time_start:.2f} с")
+# with open(keyword+".csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["_id", "brand", "name", "price", "link",])
+#     for card in cards_list:
+#         writer.writerow([card["_id"], card["brand"], card["name"], card["price"], card["link"]])
+print(f"Обработано {len(cards_list)} записей за {time.time() - time_start:.2f} с")
+time.sleep(5)
+print(f"Выгружено с БД {DB} коллекции {COLLECTION} {mongo_dump()} уникальных документов")
+print(f"Сохранено в файл {COLLECTION}.json")

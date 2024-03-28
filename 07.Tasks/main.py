f6fb0b729cbdeaac06a2035fa7299003471ae747
allseenn <allseen@live.ru>
2024-03-28 20:16:32 +0000

    молоко

--- 07.Tasks/main.py
+++ 07.Tasks/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from wildberries import *
+from auchan import *
 import time
 
 def main(keyword):
@@ -8,10 +8,12 @@ def main(keyword):
 
 if __name__ == "__main__":
     start_time = time.time()
+    print("\033c")
     keyword = input("Введите ключевое слово для поиска товаров: ")
     big_list = main(keyword)
     unique_list = db.unique(big_list)
+    print("\033c")
     db.csv_file(keyword, big_list)
     db.json_file(keyword, unique_list)
-    # db.upload("wildberries", keyword, main_list) 
+    # db.upload("auchan", keyword, main_list) 
     print(f"Время выполнения: {time.time() - start_time:.2f} секунд")

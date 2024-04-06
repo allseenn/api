9ae1bb164d1ed70a7c738f35daf7a55039471e94
allseenn <allseen@live.ru>
2024-04-06 09:09:03 +0000

    90

---
+++ 09.Tasks/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -f aclImdb_v1.tar.gz ]; then
+    echo "aclImdb_v1.tar.gz already exists"
+else
+    wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+fi
+# -e '$s/$/;/'
+tar -xzf aclImdb_v1.tar.gz
+sed -i -e 's/"//g' -e 's/<br \/>//g' -e 's/\t//g' -e 's/\n//g' -e 's/;//g' -e '$s/$/;/' -e '$a\' aclImdb/train/pos/*.txt
+cat aclImdb/train/pos/*.txt > positive.txt
+sed -i -e 's/"//g' -e 's/<br \/>//g' -e 's/\t//g' -e 's/\n//g' -e 's/;//g' -e '$s/$/;/' -e ';$a\' aclImdb/train/neg/*.txt 
+cat aclImdb/train/neg/*.txt > negative.txt
+
